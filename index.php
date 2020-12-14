c25cddef3a51df58fd5647731456cec22150801c
B1ackLabel <cast-tex@yandex.ru>
2020-12-14 17:59:40 +0300

    proj 2

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+<header>
+<title>
+  Сколько раз запросили изображение
+</title>
+</header>
+<body>
+  <img src="image.php" style="width: 1000px; height: 700px" alt="foods">
+</body>
+</html>
